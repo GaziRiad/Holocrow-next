59364bef74d56b22139da4c71b75290c2b14b1bf
GaziRiad <ouhibimustapha36@gmail.com>
2023-11-23 02:10:29 +0100

    Implemented language changing in nextjs

---
+++ contexts/LanguageContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState } from "react";
+
+const LanguageContext = createContext();
+
+function LanguageProvider({ children }) {
+  const [activeLanguage, setActiveLanguage] = useState("en");
+  return (
+    <LanguageContext.Provider value={{ activeLanguage, setActiveLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+}
+
+export function useLanguage() {
+  const context = useContext(LanguageContext);
+  if (context === undefined)
+    throw new Error("LanguageContext was used outside of LanguageProvider");
+  return context;
+}
+
+export default LanguageProvider;
--- src/app/solutions/retail/page.js
+++ src/app/solutions/retail/page.js
@@ -5,79 +5,21 @@ import Hero from "@/components/Hero";
 import MainFooter from "@/components/MainFooter";
 import FirstSection from "@/components/solutions/FirstSection";
 import ProductsSection from "@/components/solutions/ProductsSection";
+import { useLanguage } from "@/contexts/LanguageContext";
 
 import { motion } from "framer-motion";
 
-function page() {
-  const content = {
-    heroImg: "/assets/Solutions/MarketingHero.png",
-    heroHeading: "RETAIL & FMCG",
-    subHero: ["Allways On", "Billion Hours Trained", "Marketing Analyst"],
-    firstSection: {
-      content: [
-        {
-          img: "/assets/Solutions/img-7.png",
-          title: "Accurate Insights",
-          text: "Obtain visitor count, heatmap, demographic insights, and many more with 99.9% accuracy.",
-        },
-        {
-          img: "/assets/Solutions/img-8.png",
-          title: "Prevent Out of Stock",
-          text: "Detect and track every single buying transactions only via video streams to prevent stock out and gather precious insights.",
-        },
-        {
-          img: "/assets/Solutions/img-3.jpg",
-          title: "Data Visualization",
-          text: "Get live reports and alerts with easy to understand visuals.",
-        },
-      ],
-    },
+//
+import enRetail from "../../../../public/translations/en/retail.json";
+import trRetail from "../../../../public/translations/tr/retail.json";
+const translations = {
+  en: enRetail,
+  tr: trRetail,
+};
 
-    productsSection: {
-      heading: "BEYOND WATCHING",
-      subHeading: "See our all security products",
-      content: [
-        {
-          icon: "/assets/icons/19.png",
-          title: "Visitor Counting",
-          text: "Count unique visitors using anonymous IDs, excluding staff, to gauge the potential of your stores.",
-        },
-        {
-          icon: "/assets/icons/20.png",
-          title: "Demographic Targeting",
-          text: "Get the demographic diversity of your visitors to implement the correct segmentation.",
-        },
-        {
-          icon: "/assets/icons/3.png",
-          title: "A/B Testing",
-          text: "Understand customers' choices on your new products in a quick way.",
-        },
-        {
-          icon: "/assets/icons/14.png",
-          title: "Heatmap & Footfall",
-          text: "Analyze customer movement patterns and store hotspots to optimize layout and product placement.",
-        },
-        {
-          icon: "/assets/icons/4.png",
-          title: "Product Engagement Analysis",
-          text: "Monitor customer interactions with products to gain insights into preferences and engagement levels.",
-        },
-        {
-          icon: "/assets/icons/16.png",
-          title: "Inventory & Stock Control",
-          text: "Automate inventory tracking and maintain optimal stock levels using real-time visual data analysis.",
-        },
-      ],
-    },
-    thirdSection: {
-      heading: "Private Data Collection",
-      content: {
-        img: "/assets/Solutions/data-collection.jpg",
-        text: "Holocrow never collects personal identification details from the visual content we handle. Our primary goal is to enhance security while preserving people’s confidentiality.",
-      },
-    },
-  };
-
+function Retail() {
+  const { activeLanguage } = useLanguage();
+  const content = translations[activeLanguage];
   return (
     <>
       <Hero
@@ -119,4 +61,4 @@ function page() {
   );
 }
 
-export default page;
+export default Retail;
