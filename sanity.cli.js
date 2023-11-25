e1c9e908a24ae1fc43a2788d9e6b382a4d63ec6c
GaziRiad <ouhibimustapha36@gmail.com>
2023-11-25 08:33:29 +0100

    Changed config in sanityé

--- sanity.cli.js
+++ sanity.cli.js
@@ -4,9 +4,7 @@
  **/
 import { defineCliConfig } from "sanity/cli";
 
-// const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-// const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
-const projectId = "0vd70diw";
-const dataset = "production";
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export default defineCliConfig({ api: { projectId, dataset } });
