cd8be98faa84e6f348dfa45a20de398ef3bb8f6c
GaziRiad <ouhibimustapha36@gmail.com>
2024-01-01 17:25:31 +0100

    added more styling to holocrow blog page

--- src/components/BlogSlider.jsx
+++ src/components/BlogSlider.jsx
@@ -11,6 +11,8 @@ import "swiper/css/navigation";
 // import required modules
 import { Navigation } from "swiper/modules";
 import Link from "next/link";
+import { FiLink } from "react-icons/fi";
+
 function BlogSlider({ posts }) {
   const breakpoints = {
     768: {
@@ -19,7 +21,7 @@ function BlogSlider({ posts }) {
     },
     1024: {
       slidesPerView: 3,
-      spaceBetween: 120,
+      spaceBetween: 100,
     },
   };
 
@@ -33,18 +35,24 @@ function BlogSlider({ posts }) {
       breakpoints={breakpoints}
     >
       {posts?.map((post) => (
-        <SwiperSlide
-          key={post.title}
-          className=" hover:bg-primary/30 rounded-2xl pb-4"
-        >
+        <SwiperSlide key={post.title} className="group rounded-xl pb-4">
           <Link href={`/blog/${post.slug.current}`}>
+            <div className=" hidden group-hover:block absolute top-0 left-0 bg-slate-900/60  w-full h-full rounded-xl" />
+            <FiLink
+              className="hidden group-hover:block absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+              size={42}
+              color="#fff"
+            />
             <div className="flex flex-col items-center overflow-hidden h-[440px] lg:h-[520px]">
               <img
                 src={post.mainImage.asset?.url}
                 alt={post.mainImage.alt}
                 className="w-full h-72 rounded-2xl mb-4"
               />
-              <p className=" w-full font-bold text-lg text-black-800 capitalize mb-3 text-left px-4 capitalize">
+              <p className="w-full text-sm text-primary font-semibold uppercase -mb-0.5 px-4">
+                Blog Post
+              </p>
+              <p className="w-full font-bold text-lg text-black-800 mb-3 text-left px-4 capitalize">
                 {post.title}
               </p>
               <p className="text-base text-black-800 text-left px-4">
--- src/components/blog/SecondaryPosts.jsx
+++ src/components/blog/SecondaryPosts.jsx
@@ -1,15 +1,22 @@
 import Link from "next/link";
+import { FiLink } from "react-icons/fi";
 
 function SecondaryPosts({ posts }) {
   return (
-    <section className="container mx-auto flex items-center justify-between flex-col gap-12 mb-12 px-4 lg:flex-row xl:gap-36 lg:px-16 2xl:px-56 lg:mb-56">
+    <section className="container mx-auto flex items-center justify-between flex-col gap-12 mb-12 px-4 lg:flex-row xl:gap-36 lg:px-16 2xl:px-72 lg:mb-56">
       {posts.map((post) => (
         <Link
           href={`/blog/${post.slug.current}`}
           key={post.title}
-          className="w-full lg:w-1/2 "
+          className="group w-full lg:w-1/2 relative"
         >
-          <div className=" relative rounded-2xl overflow-hidden  ">
+          <div className="hidden group-hover:block absolute z-10 top-0 left-0 bg-slate-900/60  w-full h-full rounded-xl" />
+          <FiLink
+            className="hidden group-hover:block absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            size={42}
+            color="#fff"
+          />
+          <div className="relative rounded-2xl overflow-hidden">
             <img
               className="w-full h-[280px] lg:h-[360px] object-fill"
               src={post.mainImage.asset?.url}
@@ -24,10 +31,13 @@ function SecondaryPosts({ posts }) {
                 width: "100%",
                 height: "100%",
                 background:
-                  "linear-gradient(to bottom, rgba(255, 255, 255, 0.025) 50%, rgba(255, 255, 255, 1) 100%)",
+                  "linear-gradient(to bottom, #D9D9D900, #FBFBFBE4, #FFFFFF)",
               }}
             />
             <div className=" absolute bottom-0 px-8 pb-6">
+              <p className="text-sm text-primary font-semibold uppercase -mb-0.5">
+                Blog Post
+              </p>
               <p className="w-full font-bold text-xl text-black-800 capitalize mb-3 text-left capitalize">
                 {post.title}
               </p>
