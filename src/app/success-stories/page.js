59c235bfde911ed026a50e40c3a96d48cb29e9d8
GaziRiad <ouhibimustapha36@gmail.com>
2024-02-16 16:18:47 +0100

    added Fixes to success page

--- src/app/success-stories/page.js
+++ src/app/success-stories/page.js
@@ -44,13 +44,15 @@ function SuccessStories() {
         transition={{ duration: 1 }}
         className="mb-24 text-black-800"
       >
-        <section className="relative h-screen mb-12 sm:mb-24 md:-mb-32 xl:-mb-20">
+        <section className="relative mb-12 sm:mb-24 md:-mb-32 xl:-mb-20">
           <Hero herobg="hero-solutions" noPattern={true} />
+        </section>
+        <section className="">
           <motion.div
             initial={{ y: -400 }}
             animate={{ y: 0 }}
             transition={{ duration: 0.6 }}
-            className="absolute top-0 w-full h-full"
+            className="absolute top-20 w-full h-full"
           >
             <Swiper
               modules={[Pagination, Navigation]}
@@ -63,7 +65,7 @@ function SuccessStories() {
               onSlideChange={(swiper) => {
                 setCurrStory(swiper.activeIndex);
               }}
-              className="storiesSlider w-[95%] absolute top-[40%] -translate-y-1/3 sm:top-[40%] md:top-[35%] lg:w-[80%] xl:top-[45%] xl:w-[65%] shadow-2xl rounded-2xl h-[90%] md:h-[60%]"
+              className="storiesSlider w-[95%] absolute top-[40%] -translate-y-1/3 sm:top-[40%] md:top-[35%] lg:w-[80%] xl:top-[45%] xl:w-[65%] shadow-2xl rounded-2xl"
             >
               {stories.map((story, index) => {
                 return (
@@ -71,18 +73,16 @@ function SuccessStories() {
                     className={`transition-transform duration-300 transform`}
                     key={index}
                   >
-                    <div className="bg-white/50 h-full flex flex-col items-center justify-center gap-2 px-6 py-6 xl:flex-row xl:p-10 2xl:p-20 2xl:gap-20">
+                    <div className="bg-white/50 h-full flex flex-col items-center justify-center gap-2 px-6 py-8 xl:flex-row xl:p-10 2xl:p-20 2xl:gap-20">
                       <div className="xl:w-1/2 flex items-center justify-center">
                         <motion.img
                           whileHover={{
                             rotate: -2,
                             boxShadow: "10px 5px 5px #FFB800",
                           }}
-                          // height={500}
-                          // width={500}
                           src={story.mainImage.asset.url}
                           alt={story.mainImage?.alt}
-                          className="w-50 h-40 xl:h-auto xl:w-full rounded-2xl"
+                          className=" w-1/2 h-auto xl:w-full rounded-2xl"
                         />
                       </div>
                       <div className=" flex flex-col items-center xl:items-start justify-center gap-6 h-full xl:w-1/2">
@@ -91,10 +91,8 @@ function SuccessStories() {
                           animate={{ x: 0, opacity: 1 }}
                           whileHover={{ scale: 1.2 }}
                           transition={{ type: "spring", stiffness: 100 }}
-                          // height={500}
-                          // width={500}
                           src={story.logo.asset.url}
-                          className=" w-1/6 xl:w-1/4"
+                          className=" w-24"
                           alt={story?.logo?.alt}
                         />
                         <p>
@@ -111,21 +109,21 @@ function SuccessStories() {
             </Swiper>
           </motion.div>
         </section>
-        <section className=" flex flex-col items-center justify-center">
-          <p className=" text-primary text-3xl font-semibold mb-4">
-            {stories[currStory]?.client}
-          </p>
-          <p className=" flex items-center justify-center">
-            <span className="text-3xl">{"{"}</span>
-            <span className="px-2 text-lg lg:text-xl">
-              {stories[currStory]?.year}
-              {" / "}
-              {stories[currStory]?.location}
-            </span>
-            <span className="text-3xl">{"}"}</span>
-          </p>
-        </section>
       </motion.div>
+      <section className="mb-24 mt-32 lg:mt-20 flex flex-col items-center justify-center">
+        <p className=" text-primary text-3xl font-semibold mb-4">
+          {stories[currStory]?.client}
+        </p>
+        <p className=" flex items-center justify-center">
+          <span className="text-3xl">{"{"}</span>
+          <span className="px-2 text-lg lg:text-xl">
+            {stories[currStory]?.year}
+            {" / "}
+            {stories[currStory]?.location}
+          </span>
+          <span className="text-3xl">{"}"}</span>
+        </p>
+      </section>
     </AnimatePresence>
   );
 }
